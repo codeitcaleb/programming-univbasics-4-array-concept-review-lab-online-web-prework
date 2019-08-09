4be6f484738e3f510b459a9b3ea0da62b7ca5866
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-09 15:00:46 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,18 @@
 def find_element_index(array, value_to_find)
-  array.index(array, value_to_find)
+  
+      array.length.times do |counter|
+      if array[counter] == value_to_find
+      return counter
+    end
+  end   
+  
+  nil 
+end
 
 def find_max_value(array)
-  puts array.max 
+  array.max 
 end
 
 def find_min_value(array)
-  puts array.min
+  array.min
 end
