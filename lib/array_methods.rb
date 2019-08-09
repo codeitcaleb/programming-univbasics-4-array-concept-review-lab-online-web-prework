b3d47eb125074b48565cdab2bf8f1f2df9e30450
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-09 14:46:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   index = 0
   
-  while index < value_to_find do 
-     current_value = array[index]
+      array.length.times do |counter|
+      if array[counter] == value_to_find
+      current_value = array.length.times[index]
      index += 1 
      
      return |array|.index(current_value)
