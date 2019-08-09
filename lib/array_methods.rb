bb6bd59b77620d55aa81ff72be1628858b0ae4d3
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-09 14:53:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,9 +10,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  puts array.max 
+  array.max 
 end
 
 def find_min_value(array)
-  puts array.min
+  array.min
 end
