a29312a6c6649be4ea9bb544ed33cd72bb2430e7
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-09 13:47:08 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  array.index(array, value_to_find)
 
 def find_max_value(array)
-  # Add your solution here
+  puts array.max 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  puts array.min
 end
